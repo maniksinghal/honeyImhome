6041b29e3e6439ae95c97fa50d49f501ae1d8e33
maniksinghal <manik.singhal@gmail.com>
2016-06-12 10:43:02 +0530

    Speech rate control

--- app/src/main/java/com/wordpress/randomexplorations/honeyimhome/MainActivity.java
+++ app/src/main/java/com/wordpress/randomexplorations/honeyimhome/MainActivity.java
@@ -717,6 +717,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
 
         //noinspection SimplifiableIfStatement
